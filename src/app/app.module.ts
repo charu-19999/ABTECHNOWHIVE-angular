c33126e27c4d338d69798276b02bca9ab3bbd42c
charu-19999 <charu.gupta@greatwits.com>
2022-02-17 21:29:42 +0530

    first commit with angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeroComponent } from './hero/hero.component';
+
+@NgModule({
+  declarations: [AppComponent, HeaderComponent, FooterComponent, HeroComponent],
+  imports: [BrowserModule, NgbModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
